ff44ff13cde7f6bf22df0c568b4d8cdfe7215aaf
Teklee <kassateklee@gmail.com>
2022-11-25 12:20:39 -0800

    function intro

---
+++ play.js
@@ -0,0 +1,8 @@
+var  name='tekle';
+var age=23;
+var hobbies='soccer';
+ function summarizUser(userName,userAge,userHobbies){
+  return('name:'+ userName + ',age:'+ userAge +',hobbies:'+ userHobbies)
+ }
+
+ console.log(summarizUser(name,age,hobbies))
